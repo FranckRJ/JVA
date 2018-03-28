bb166ec932bb69869bc71e6e4779e9b6e4446a70
FranckRJ <franck.rj.w@gmail.com>
2018-03-28 22:19:37 +0200

    Amélioration de la gestion des espacement avec la navbar/statusbar.

--- kotlin/com/franckrj/jva/MainActivity.kt
+++ kotlin/com/franckrj/jva/MainActivity.kt
@@ -2,6 +2,8 @@ package com.franckrj.jva
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -14,16 +16,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView: RecyclerView = findViewById(R.id.message_list_main)
-        val listAdapter = TopicAdapter()
+        val idOfNavBarHeight: Int = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+        val navBarHeight: Int = if (idOfNavBarHeight > 0) resources.getDimensionPixelSize(idOfNavBarHeight) else 0
+        val idOfStatusBarHeight: Int = resources.getIdentifier("status_bar_height", "dimen", "android")
+        val statusBarHeight: Int = if (idOfStatusBarHeight > 0) resources.getDimensionPixelSize(idOfStatusBarHeight) else 0
+        val defaultMessageListPadding = resources.getDimensionPixelSize(R.dimen.messageListPadding)
+        val messageListView: RecyclerView = findViewById(R.id.message_list_main)
+        val messageListAdapter = TopicAdapter()
+        var navBarIsInApp = resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
 
-        listView.layoutManager = LinearLayoutManager(this)
-        listView.adapter = listAdapter
+        if (Build.VERSION.SDK_INT >= 24) {
+            if (isInMultiWindowMode) {
+                navBarIsInApp = false
+            }
+        }
+
+        messageListView.setPaddingRelative(defaultMessageListPadding,
+                                           defaultMessageListPadding + statusBarHeight,
+                                           defaultMessageListPadding,
+                                           defaultMessageListPadding + if (navBarIsInApp) navBarHeight else 0)
+
+        messageListView.layoutManager = LinearLayoutManager(this)
+        messageListView.adapter = messageListAdapter
         topicViewModel = ViewModelProviders.of(this).get(TopicViewModel::class.java)
 
         topicViewModel?.getListOfMessagesShowable()?.observe(this, Observer { listOfMessagesShowable ->
-            listAdapter.listOfMessagesShowable = listOfMessagesShowable ?: ArrayList()
-            listAdapter.notifyDataSetChanged()
+            messageListAdapter.listOfMessagesShowable = listOfMessagesShowable ?: ArrayList()
+            messageListAdapter.notifyDataSetChanged()
         })
     }
 }
