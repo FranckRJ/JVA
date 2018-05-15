c48d783001a820f34d0cf222ca8ed87dfca76a90
Franck W <franck.rj.w@gmail.com>
2018-05-15 17:59:35 +0200

    Mise en place d'un bouton retour fonctionnel.

--- kotlin/com/franckrj/jva/topic/ViewTopicActivity.kt
+++ kotlin/com/franckrj/jva/topic/ViewTopicActivity.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.view.ViewPager
+import android.widget.ImageView
 import com.franckrj.jva.R
 import com.franckrj.jva.base.CollapsibleToolbarActivity
 import com.franckrj.jva.pagenav.NavigationUtils
@@ -18,12 +19,17 @@ class ViewTopicActivity : CollapsibleToolbarActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_viewtopic)
 
+        val arrowBackButton: ImageView = findViewById(R.id.arrow_back_button_viewtopic)
         val topicViewPager: ViewPager = findViewById(R.id.topic_pager_viewtopic)
         val topicNavigation = PageNavigationHelper(topicViewPager, { ViewTopicPageFragment() }, supportFragmentManager)
         val topicViewModel: TopicViewModel = ViewModelProviders.of(this).get(TopicViewModel::class.java)
 
         initToolbar(topicViewPager, findViewById(R.id.appbar_layout_viewtopic), findViewById(R.id.toolbar_layout_viewtopic),
                     findViewById(R.id.toolbar_card_viewtopic), findViewById(R.id.title_text_toolbar_viewtopic), findViewById(R.id.subtitle_text_toolbar_viewtopic))
+
+        arrowBackButton.setOnClickListener {
+            onBackPressed()
+        }
 
         NavigationUtils.initContentViewPagerNavigation(this, topicViewPager, topicNavigation, topicViewModel)
 
